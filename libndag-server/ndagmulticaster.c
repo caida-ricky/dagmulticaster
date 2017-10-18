a457274f1e4b88c2e999e8c72896c7b86bef0e90
Shane Alcock <salcock@waikato.ac.nz>
2017-10-18 15:11:46 +1300

    Added ability to create beaconer thread and form beacons
    The beacons are not yet transmitted.

--- libndag-server/ndagmulticaster.c
+++ libndag-server/ndagmulticaster.c
@@ -6,8 +6,16 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <errno.h>
+#include <unistd.h>
 
 #include "ndagmulticaster.h"
+
+volatile int halted = 0;
+
+int ndag_interrupt_beacon(void) {
+    halted = 1;
+    return halted;
+}
 
 int ndag_create_multicaster_socket(uint16_t port, char *groupaddr) {
 
@@ -67,6 +75,75 @@ int ndag_send_encap_libtrace(int sock, libtrace_packet_t *packet) {
 
     /* TODO actually construct and send datagrams to the multicast socket! */
     return 1;
+}
+
+
+static uint32_t construct_beacon(char **buffer, ndag_beacon_params_t *nparams) {
+
+    uint32_t beacsize = sizeof(ndag_common_t) +
+            (sizeof(uint16_t) * (nparams->numstreams + 1));
+
+    char *beac = (char *)malloc(beacsize);
+    ndag_common_t *hdr;
+    int i;
+    uint16_t *next;
+
+    if (beacsize > NDAG_MAX_DGRAM_SIZE) {
+        fprintf(stderr, "Beacon is too large to fit in a single datagram!\n");
+        free(beac);
+        return 0;
+    }
+
+    hdr = (ndag_common_t *)beac;
+    hdr->magic = htonl(NDAG_MAGIC_NUMBER);
+    hdr->version = NDAG_EXPORT_VERSION;
+    hdr->type = NDAG_PKT_BEACON;
+    hdr->monitorid = htons(nparams->monitorid);
+
+    next = (uint16_t *)(beac + sizeof(ndag_common_t));
+
+    *next = htons(nparams->numstreams);
+    next ++;
+
+    for (i = 0; i < nparams->numstreams; i++) {
+        *next = htons(nparams->streamports[i]);
+        next++;
+    }
+
+    *buffer = beac;
+    return beacsize;
+
+}
+
+void *ndag_start_beacon(void *params) {
+
+    ndag_beacon_params_t *nparams = (ndag_beacon_params_t *)params;
+    int beacsock;
+    char *beaconrec = NULL;
+    uint32_t beacsize = 0;
+
+    beacsock = ndag_create_multicaster_socket(nparams->beaconport,
+            nparams->groupaddr);
+
+    if (beacsock == -1) {
+        fprintf(stderr, "Failed to create multicast socket for beacon thread.\n");
+        pthread_exit(NULL);
+    }
+
+    beacsize = construct_beacon(&beaconrec, nparams);
+
+    while (beacsize > 0 && beaconrec != NULL && !halted) {
+
+        fprintf(stderr, "Sending beacon...\n");
+        usleep(nparams->frequency * 1000);
+    }
+
+    if (beaconrec) {
+        free(beaconrec);
+    }
+    close(beacsock);
+    pthread_exit(NULL);
+
 }
 
 
--- libndag-server/ndagmulticaster.h
+++ libndag-server/ndagmulticaster.h
@@ -5,16 +5,57 @@
 #include <sys/types.h>
 #include <libtrace.h>
 
-#define NDAG_MAX_DGRAM_SIZE 8900
+#define NDAG_MAX_DGRAM_SIZE (8900)
+
+#define NDAG_MAGIC_NUMBER (0x4E444147)
+#define NDAG_EXPORT_VERSION 1
+
+/* Note: libtrace.h provides PACKED for us... */
 
 /* Required for multicasting NDAG records */
+
+typedef struct ndagbeaconparams {
+    char *groupaddr;
+    uint16_t beaconport;
+    uint16_t numstreams;
+    uint16_t *streamports;
+    uint32_t frequency;
+    uint16_t monitorid;
+} ndag_beacon_params_t;
+
+enum {
+    NDAG_PKT_BEACON = 0x01,
+    NDAG_PKT_ENCAPERF = 0x02,
+    NDAG_PKT_RESTARTED = 0x03,
+    NDAG_PKT_ENCAPRT = 0x04,
+};
+
+/* == Protocol header structures == */
+
+/* Common header -- is prepended to all exported records */
+typedef struct ndag_common_header {
+    uint32_t magic;
+    uint8_t version;
+    uint8_t type;
+    uint16_t monitorid;
+} PACKED ndag_common_t;
+
+/* Beacon -- structure is too simple to be worth defining as a struct */
+/*
+ * uint16_t numberofstreams;
+ * uint16_t firststreamport;
+ * uint16_t secondstreamport;
+ * ....
+ * uint16_t laststreamport;
+ */
+
+int ndag_interrupt_beacon(void);
+void *ndag_start_beacon(void *params);
 int ndag_create_multicaster_socket(uint16_t port, char *groupaddr);
 int ndag_send_encap_records(int sock, char *buf, uint32_t tosend,
         uint16_t reccount);
 int ndag_send_encap_libtrace(int sock, libtrace_packet_t *packet);
 
-
-/* TODO define NDAG protocol headers in here */
 
 #endif
 // vim: set sw=4 tabstop=4 softtabstop=4 expandtab :
